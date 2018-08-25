1cc23dc43a5de70754c369d7f3db3f41c7619844
luizpaulofranz <luizpaulofranz@gmail.com>
2018-08-25 00:11:46 -0300

    Adds our first service worker event listeners.

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,14 @@
-/** My SW */
+/** Adding Some Events */
+self.addEventListener('install', function(event) {
+    console.log('[Service Worker] Installing Service Worker');
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('[Service Worker] Activating Service Worker');
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log('[Service Worker] Fetching Something');
+    event.respondWith(fetch(event.request));
+});
