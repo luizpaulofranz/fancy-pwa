25fdd99651828f973ddfe9a306f3735ab53cd4ff
luizpaulofranz <luizpaulofranz@gmail.com>
2018-08-25 00:12:56 -0300

    Handle when display the installation banner.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,4 @@
+// REGISTER OUR SW!
 // check if the browser supports serviceWorker
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
@@ -6,3 +7,12 @@ if ('serviceWorker' in navigator) {
             console.log('Service Worker registered!');
         });
 }
+// trate it to show the install banner when we click on specific button
+// so here we cancel the default show banner event to handle ir latter
+var deferredPrompt;
+window.addEventListener('beforeinstallprompt', function(){
+    console.log('beforeinstallprompt was fired.');
+    event.preventDefault()
+    deferredPrompt = event;
+    return false;
+})
--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -2,8 +2,24 @@ var shareImageButton = document.querySelector('#share-image-button');
 var createPostArea = document.querySelector('#create-post');
 var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
 
+// we add here the code to show our install banner
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
+  // the banner is only possible to be showed after the browser tries to do it
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then(choiceResult => {
+      console.log(choiceResult.outcome);
+
+      if(choiceResult.outcome === "dismiss") {
+        console.log('User cancel installation');
+      } else {
+        console.log('User ADD into home screen!');
+      }
+
+      deferredPrompt = null;
+    });
+  }
 }
 
 function closeCreatePostModal() {
