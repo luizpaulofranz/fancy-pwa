5f39cba014cd67b86fc66fa6f8d8f5c5f584433b
luizpaulofranz <luizpaulofranz@gmail.com>
2018-08-31 14:08:17 -0300

    Adds pre-caching with addAll(), adding all the home-page static contents

--- public/sw.js
+++ public/sw.js
@@ -8,9 +8,22 @@ self.addEventListener('install', function(event) {
         caches.open('static') //static is the name of this cache
             .then(cache => {
                 console.log('[Service Worker] Precaching App Shell');
-                cache.add('/src/js/app.js')
-                cache.add('/')
-                cache.add('/index.html')
+                // we only pre-cache the home-page files
+                cache.addAll([
+                    '/',
+                    '/src/js/app.js',
+                    '/src/js/feed.js',
+                    '/src/js/promise.js',
+                    '/src/js/fetch.js',
+                    '/src/js/material.min.js',
+                    '/src/css/app.css',
+                    '/src/css/feed.css',
+                    '/src/images/main-image.jpg',
+                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+                ]);
+                //cache.add('/index.html')
             })
     )
 });
